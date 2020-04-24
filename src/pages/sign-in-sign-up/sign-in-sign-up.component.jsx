b1449b6a9975900076ff2efe9516a4f5d4811ef3
brthzz <brthzz@gmail.com>
2020-04-24 15:05:46 +0200

    sign-in component styling done, custom-button

---
+++ src/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './sign-in-sign-up.style.scss';
+
+import SignIn from '../../components/sign-in/sign-in.component';
+
+
+const SignInSignUpPage = () => (
+    <SignIn />
+)
+
+export default SignInSignUpPage; 
