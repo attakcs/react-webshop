55723cb76fc74359123bb219b2c24862700273c3
brthzz <brthzz@gmail.com>
2020-05-02 15:59:54 +0200

    implemented userReducer and userActions to header and app components

--- src/App.js
+++ src/App.js
@@ -9,36 +9,32 @@ import Header from './components/header/header.component';
 
 import { createUserProfileDocument, auth } from './firebase/firebase.utils';
 
+import { connect } from "react-redux";
+import { setCurrentUser } from "./redux/user/user.actions";
+
 
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null
-    }
-  }
 
   // google firebase authentication --->
   unsubscribeFromAuth = null
   // listening to state changes
   // need to "pass back" the userRef, if we wanna use it somewhere else...
   componentDidMount() {
+    const {setCurrentUser} = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if(userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState(
-            {
-              currentUser: {
+          setCurrentUser({
                 id: snapShot.id,
                 ...snapShot.data()
-              }
           });
         });
       }
-      this.setState({ currentUser: userAuth });
+      
+      setCurrentUser(userAuth);
     });
   }
 
@@ -54,7 +50,7 @@ class App extends React.Component {
     return (
       <div>
         {/* whatever you put outside of switch, will stay there ...eg header,footer... */}
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route exact path='/shop' component={ShopPage} />
@@ -65,4 +61,8 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(null, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
+// REDUX -->
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 ReactDOM.render(
-  <Provider>
+  <Provider store={store}>
     <Router>
       <App />
     </Router>
